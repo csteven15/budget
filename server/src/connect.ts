90a685e7ea5ab1e02ade092007d3115124d2075e
Steven Chen <chensteven8899@gmail.com>
2020-11-22 16:31:12 -0500

    Added Mongoose and User CRUD

---
+++ server/src/connect.ts
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+type TInput = {
+  db: string;
+};
+
+export default ({db}: TInput) => {
+  const connect = () => {
+    mongoose
+      .connect(
+        db,
+        {
+          useNewUrlParser: true,
+          useCreateIndex: true,
+          useUnifiedTopology: true
+        }
+      )
+      .then(() => {
+        return console.info(`Successfully connected to ${db}`);
+      })
+      .catch(error => {
+        console.error(`Error connecting to database: `, error);
+      });
+  };
+  connect();
+
+  mongoose.connection.on("disconnected", connect);
+}
