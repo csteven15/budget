00212f529b4243531d02933cd3f372b699a936b0
Steven Chen <chensteven8899@gmail.com>
2020-11-22 02:04:09 -0500

    Added cors and body-parser

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,12 @@
+import bodyParser from "body-parser";
+import cors from "cors";
 import express from "express";
 import http from "http";
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
 
 app.use("/", (req, res) => {
   res.send("Hello World")
