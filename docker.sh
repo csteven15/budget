78fce6609f15cb0222f8e1084ca67fdcefdc98c6
Steven Chen <chensteven8899@gmail.com>
2021-01-20 07:30:09 +0000

    Updated scripts and added environment files

--- docker.sh
+++ docker.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
 # Run docker-compose and starts mongo-express
+set -a
+. ./production.env
+. ./development.env
+set +a
 
-cmd="docker-compose up --build --force-recreate --no-deps &"
-echo "$cmd" && eval "$cmd"
+cmd="docker-compose up --build --force-recreate --no-deps"
+echo "$cmd" && $cmd
 
 echo "Starting Mongo Express"
+
 cmd="./mongo-express.sh &"
-echo "$cmd" && eval "$cmd"
+echo "$cmd" && $cmd
--- mongo-express.sh
+++ mongo-express.sh
@@ -1,32 +1,40 @@
 #!/bin/sh
 
 # Starts mongo-express
+set -a
+. ./production.env
+. ./development.env
+set +a
 
 if [ "$#" -eq 0 ]; then
   environment="Production"
-  db_container_name="mongo"
-  container_name="mongo-express"
-  port=27018
+  db_container_name=$MONGO_PROD_CONTAINER
+  container_name=$MONGO_EXPRESS_PROD_CONTAINER
+  port=$MONGO_PROD_CONTAINER_PORT
+  bind_port=$MONGO_EXPRESS_PROD_CONTAINER_BIND_PORT
+  network=$DOCKER_PROD_NETWORK
 else
   environment="Development"
-  db_container_name="mongo-dev"
-  container_name="mongo-express-dev"
-  port=27017
+  db_container_name=$MONGO_DEV_CONTAINER
+  container_name=$MONGO_EXPRESS_DEV_CONTAINER
+  port=$MONGO_DEV_CONTAINER_PORT
+  bind_port=$MONGO_EXPRESS_DEV_CONTAINER_BIND_PORT
+  network=$DOCKER_DEV_NETWORK
 fi
 
 echo "Environment: $environment"
 
-if [ $( docker ps -a -f name=$container_name | wc -l ) -eq 2 ]; then
+if [ "$(docker container inspect -f '{{.Id}}' $container_name | wc -l)" -eq 2 ]; then
   echo "Container $container_name exists. Restarting..."
-  docker stop $container_name || true
+  docker container stop $container_name || true
   docker container start $db_container_name
 else
   echo "Creating container $container_name"
   docker run --name $container_name \
-    --network budget \
+    --network $network \
     -e ME_CONFIG_MONGODB_SERVER=$db_container_name \
-    -e ME_CONFIG_BASICAUTH_USERNAME=budget \
-    -e ME_CONFIG_BASICAUTH_PASSWORD=pass \
+    -e ME_CONFIG_BASICAUTH_USERNAME=$MONGODB_USERNAME \
+    -e ME_CONFIG_BASICAUTH_PASSWORD=$MONGODB_PASSWORD \
     -e ME_CONFIG_MONGODB_PORT=$port \
-    -p 8081:8081 mongo-express
+    -p 8081:$bind_port mongo-express
 fi
