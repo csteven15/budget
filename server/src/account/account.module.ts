de5d9f0bd19ea9585e47fad4c452cdf408c5a86f
Steven Chen <chensteven8899@gmail.com>
2021-03-11 21:24:33 -0500

    Add resolver to account module

--- server/src/account/account.module.ts
+++ server/src/account/account.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AccountService } from './account.service';
 import { Account, AccountSchema } from './account.schema';
+import { AccountResolver } from './account.resolver';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }]),
   ],
-  providers: [AccountService],
+  providers: [AccountService, AccountResolver],
   exports: [AccountModule],
 })
 export class AccountModule {}
