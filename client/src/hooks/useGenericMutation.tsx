207b67e4a115ea8389fe343f0def8e0b1f889aa3
Steven Chen <chensteven8899@gmail.com>
2021-04-10 15:20:37 -0400

    Fixed refreshing due to cache invalidation on generic mutation

--- client/src/hooks/useGenericMutation.tsx
+++ client/src/hooks/useGenericMutation.tsx
@@ -1,19 +1,10 @@
 import request from 'graphql-request'
 import { Variables } from 'graphql-request/dist/types'
-import { useMutation, useQueryClient } from 'react-query'
+import { useMutation } from 'react-query'
 import { endpoint } from '../util/Api'
 
 export const useGenericMutation = (mutationSchema: string) => {
-  const queryClient = useQueryClient()
-
-  return useMutation(
-    (variables: Variables) => request(endpoint, mutationSchema, variables),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries('entries')
-        queryClient.invalidateQueries('amounts')
-        queryClient.invalidateQueries('accounts')
-      },
-    }
+  return useMutation((variables: Variables) =>
+    request(endpoint, mutationSchema, variables)
   )
 }
