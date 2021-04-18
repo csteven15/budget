22ae104ce51defd81b5f7b8989c6befd2f8c5954
Steven Chen <chensteven8899@gmail.com>
2021-04-18 22:56:40 +0000

    Update compose up script

--- compose-up.sh
+++ compose-up.sh
@@ -8,5 +8,5 @@ set +a
 dev_network=$DOCKER_DEV_NETWORK
 prod_network=$DOCKER_PROD_NETWORK
 
-cmd="docker-compose up --build --force-recreate --no-deps"
+cmd="docker-compose up --build"
 echo "$cmd" && $cmd
